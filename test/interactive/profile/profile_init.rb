e3fbc9f7894618cc818ee28f1846ac5f186ab20a
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-17 18:32:06 -0600

    Profile measurement is improved

--- test/interactive/profile/profile_init.rb
+++ test/interactive/profile/profile_init.rb
@@ -4,19 +4,39 @@ require 'ruby-prof'
 require 'ruby-prof-flamegraph'
 
 module Profile
-  def self.measure(&block)
-    Iterations::Warmup.get.times &block
-
+  def self.measure(batch: nil, &block)
     iterations = Iterations.get
+    warmup_iterations = Iterations::Warmup.get
+
+    if batch == true
+      batch_size = Batch.size
+
+      warmup_iterations /= batch_size
+      iterations /= batch_size
+    end
+
+    warmup_iterations.times do
+      block.(batch_size)
+    end
 
     prof_result = RubyProf.profile do
       iterations.times do
-        block.()
+        block.(batch_size)
       end
     end
 
     printer = RubyProf::FlameGraphPrinter.new prof_result
     printer.print $stdout, Hash.new
+  end
+
+  module Batch
+    def self.size
+      batch_size = ENV['BATCH_SIZE']
+
+      return batch_size.to_i if batch_size
+
+      20
+    end
   end
 
   module Iterations
@@ -40,7 +60,7 @@ module Profile
   end
 
   module ReadStream
-    def self.get
+    def self.get(session=nil)
       stream = ENV['STREAM']
 
       if stream.nil?
