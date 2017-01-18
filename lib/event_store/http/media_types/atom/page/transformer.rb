b4172bc0a54296b5624c906404f25091463d3079
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-17 20:30:56 -0600

    Atom page transform performance is improved

--- lib/event_store/http/media_types/atom/page/transformer.rb
+++ lib/event_store/http/media_types/atom/page/transformer.rb
@@ -38,9 +38,22 @@ module EventStore
             end
 
             module JSON
-              def self.read(json_text)
-                formatted_data = ::JSON.parse json_text, symbolize_names: true
-                Casing::Underscore.(formatted_data)
+              class ObjectClass < Hash
+                def []=(raw_key, value)
+                  key = key_cache[raw_key]
+
+                  super key, value
+                end
+
+                def key_cache
+                  @@key_cache ||= Hash.new do |cache, raw_key|
+                    cache[raw_key] = Casing::Underscore.(raw_key).to_sym
+                  end
+                end
+              end
+
+              def self.read(text)
+                ::JSON.parse text, object_class: ObjectClass
               end
             end
           end
