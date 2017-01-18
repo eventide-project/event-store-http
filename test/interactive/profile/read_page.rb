e5015e8c02fd2be84b83461c87d44029dff20a29
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-17 20:31:10 -0600

    Profile tests are improved

--- test/interactive/profile/read_page.rb
+++ test/interactive/profile/read_page.rb
@@ -2,28 +2,11 @@ require_relative './profile_init'
 
 session = InteractiveTests::Session.get
 
-stream = ENV['STREAM']
-
-if stream.nil?
-  batch_size = InteractiveTests::Stream.batch_size
-
-  stream = Controls::Stream.example
-
-  Controls::Write.(events: batch_size, stream: stream, session: session)
-end
+stream = Profile::ReadStream.get
 
 read_stream = EventStore::HTTP::ReadStream.build session: session
-read_event = EventStore::HTTP::ReadEvent.build session: session
+read_stream.enable_rich_embed
 
-prof_result = RubyProf.profile do
-  atom_page = read_stream.(stream)
-
-  atom_page.entries.each do |event|
-    event_uri = event.links.fetch :edit
-
-    read_event.(event_uri)
-  end
+Profile.measure do
+  read_stream.(stream, batch_size: Profile::Batch.size)
 end
-
-printer = RubyProf::FlameGraphPrinter.new prof_result
-printer.print $stdout, Hash.new
---
+++ test/interactive/profile/read_page_and_events.rb
@@ -0,0 +1,18 @@
+require_relative './profile_init'
+
+session = InteractiveTests::Session.get
+
+stream = Profile::ReadStream.get
+
+read_stream = EventStore::HTTP::ReadStream.build session: session
+read_event = EventStore::HTTP::ReadEvent.build session: session
+
+Profile.measure batch: true do |batch_size|
+  atom_page = read_stream.(stream, batch_size: batch_size)
+
+  atom_page.entries.each do |event|
+    event_uri = event.links.fetch :edit
+
+    read_event.(event_uri)
+  end
+end
