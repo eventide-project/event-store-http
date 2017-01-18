ef1fb2ed54fc5515da3ca18ea1eb74a1e25d9332
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-17 21:15:32 -0600

    Split embed=rich profiles

--- test/interactive/profile/read_page__embed_rich.rb
+++ test/interactive/profile/read_page__embed_rich.rb
@@ -5,6 +5,7 @@ session = InteractiveTests::Session.get
 stream = Profile::ReadStream.get
 
 read_stream = EventStore::HTTP::ReadStream.build session: session
+read_stream.enable_rich_embed
 
 Profile.measure do
   read_stream.(stream, batch_size: Profile::Batch.size)
